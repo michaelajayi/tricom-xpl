0b2d8b3fdabbdf9bba9570ecdcb879de6d2a3c83
Michael Ajayi <ajayimichael@hotmail.com>
2023-05-27 00:15:29 +0100

    Done Mobile Navigation Menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 
-import { CgClose, CgMenuRight } from "react-icons/cg";
-import { TfiClose } from "react-icons/tfi";
-
 import logo from "../assets/img/logo.svg";
+import Hamburger from "./Hamburger";
+import MobileMenu from "./MobileMenu";
+
+import { useSelector } from "react-redux";
+
+import { selectLayout } from "../../app/features/layout/layoutSlice";
 
 const Nav = () => {
+  const { toggleHamburger } = useSelector(selectLayout);
+
   const navLinks = [
     { name: "Home", link: "/" },
     { name: "About Us", link: "/about-us" },
-    { name: "Meet the team", link: "/meet-the-team" },
+    // { name: "Meet the team", link: "/meet-the-team" },
     { name: "Services", link: "/services" },
     { name: "Contact Us", link: "/contact-us" },
   ];
 
-  const [openMobileMenu, setOpenMobileMenu] = useState(false);
   return (
-    <div className='w-full h-auto flex justify-center bg-white'>
-      <div className='flex lg:flex-row justify-between lg:items-start items-center py-[2rem] w-full lg:w-[90%] px-5 lg:px-0'>
+    <div className='w-full h-auto flex justify-center bg-white bg-fixed'>
+      <div className='nav flex lg:flex-row justify-between lg:items-start items-center py-[2rem] w-full lg:w-[90%] px-5 lg:px-0'>
         <a href='/'>
           <img src={logo} alt='logo' width={150} />
         </a>
@@ -32,21 +36,15 @@ const Nav = () => {
             </a>
           ))}
         </div>
-        {/* toggle mobile menu */}
-        <div className='cursor-pointer block lg:hidden'>
-          {openMobileMenu ? (
-            <CgClose
-              size={30}
-              className='text-theme-yellow'
-              onClick={() => setOpenMobileMenu(!openMobileMenu)}
-            />
-          ) : (
-            <CgMenuRight
-              size={30}
-              className='text-theme-yellow'
-              onClick={() => setOpenMobileMenu(!openMobileMenu)}
-            />
-          )}
+        <div className='lg:hidden z-40'>
+          <Hamburger />
+        </div>
+        <div
+          className={`absolute ${
+            toggleHamburger ? "h-full opacity-1" : "h-0 opacity-0"
+          } w-screen top-0 left-0 bg-gray-900 z-30 transition-all duration-[400ms] ease-in-out`}
+        >
+          <MobileMenu />
         </div>
       </div>
     </div>
